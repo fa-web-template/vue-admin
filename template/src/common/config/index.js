e830c5b42f966463e77daf31ce7f967c1d612587
gd4ark <gd.4ark@gmail.com>
2019-07-04 23:27:34 +0800

    update config

--- template/src/common/config/index.js
+++ template/src/common/config/index.js
@@ -3,7 +3,7 @@ export default {
     app_name: process.env.APP_NAME,
     app_title: process.env.APP_TITLE,
     app_version: process.env.APP_VERSION,
-    app_copyright: 'Powered By 零壹进制网络',
-    dev_server_url: `http://test.01snt.com:82/admin`,
+    app_copyright: '© 2019 4Ark. 版权所有',
+    dev_server_url: `http://test.com:82/admin`,
     server_url: `http://${location.hostname}:82/admin`
 }
