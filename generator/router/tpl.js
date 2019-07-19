3dc6827ce9c1e4fcec7280cbb0f201f5c3831279
gd4ark <gd4ark@gmail.com>
2019-07-19 09:23:50 +0800

    feat(update generator):

--- generator/router/tpl.js
+++ generator/router/tpl.js
@@ -1,10 +1,26 @@
+import { toRoles } from "@/common/utils/role"
 export default [
     {
         path: "/{{ name }}",
-        component: () => import("@/pages/admin/views/{{ name }}"),
+        component: () => import("@/common/layouts/Home"),
+        redirect: "/users/list",
         name: "{{ name }}",
         meta: {
-            title: "{{ title }}"
-        }
+            roles: toRoles("{{ roles }}"),
+            title: "{{ title }}",
+            icon: "el-icon-ali-icon_yuangongguanli"
+        },
+        children: [
+            {
+                path: "list",
+                component: () => import("@/pages/{{ page }}/views/{{ name }}"),
+                name: "{{ title }}List",
+                meta: {
+                    icon: "el-icon-ali-yonghu",
+                    roles: toRoles("{{ roles }}"),
+                    title: "{{ title }}"
+                }
+            }
+        ]
     }
 ]
