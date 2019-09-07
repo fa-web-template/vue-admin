b4d76fa187daf6d415e1f097bfe6e4255b997230
gd4ark <gd4ark@gmail.com>
2019-09-07 17:38:51 +0800

    fix(2 space):

--- template/src/common/utils/readFile.js
+++ template/src/common/utils/readFile.js
@@ -6,36 +6,35 @@
  * @returns {Object}
  */
 export function fileListToObject(modulesFiles) {
-    return modulesFiles.keys().reduce((modules, modulePath) => {
-      // set './app.js' => 'app'
-      const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
-      const value = modulesFiles(modulePath)
-      modules[moduleName] = value.default
-      return modules
-    }, {})
+  return modulesFiles.keys().reduce((modules, modulePath) => {
+    // set './app.js' => 'app'
+    const moduleName = modulePath.replace(/^\.\/(.*)\.\w+$/, '$1')
+    const value = modulesFiles(modulePath)
+    modules[moduleName] = value.default
+    return modules
+  }, {})
+}
+
+const sortKey = (keys, sorts) => {
+  const set = new Set([...sorts, ...keys])
+  return [...set]
+}
+
+/**
+ *
+ *
+ * @export
+ * @param {*} modulesFiles
+ * @param {*} sorts = null
+ * @returns {Array}
+ */
+export function fileListToArray(modulesFiles, sorts = null) {
+  let keys = modulesFiles.keys()
+  if (sorts) {
+    keys = sortKey(keys, sorts)
   }
-  
-  const sortKey = (keys, sorts) => {
-    const set = new Set([...sorts, ...keys])
-    return [...set]
-  }
-  
-  /**
-   *
-   *
-   * @export
-   * @param {*} modulesFiles
-   * @param {*} sorts = null
-   * @returns {Array}
-   */
-  export function fileListToArray(modulesFiles, sorts = null) {
-    let keys = modulesFiles.keys()
-    if (sorts) {
-      keys = sortKey(keys, sorts)
-    }
-    return keys.map(modulePath => {
-      const value = modulesFiles(modulePath)
-      return value.default[0]
-    }, {})
-  }
-  
+  return keys.map(modulePath => {
+    const value = modulesFiles(modulePath)
+    return value.default[0]
+  }, {})
+}
