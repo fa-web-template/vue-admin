ff13dddc21da339ec2689fc45078311b4468482f
gd4ark <gd.4ark@gmail.com>
2019-10-23 17:55:03 +0800

    fix(config): update dev base url

--- template/src/common/config/index.js
+++ template/src/common/config/index.js
@@ -3,7 +3,7 @@ export const app_name = process.env.APP_NAME
 export const app_title = process.env.APP_TITLE
 export const app_version = process.env.APP_VERSION
 export const app_copyright = 'Powered By 4Ark'
-export const dev_base_url = `http://localhost/my-amazing/amazing-re/public/admin`
+export const dev_base_url = `http://server.com/admin`
 export const base_url = `http://${location.hostname}:82/admin`
 
 export default {
